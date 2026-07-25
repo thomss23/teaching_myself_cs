a543c56ee0534452c430792b0ca949cf3259ba57
ThomasB <borbelithomas@gmail.com>
2026-07-25 18:01:42 +0000

    add the assembly solution to the cone volume problem

--- computer_systems/assembly/cone-volume/cone.asm
+++ computer_systems/assembly/cone-volume/cone.asm
@@ -1,6 +1,16 @@
 default rel
 
+section .data
+pi: dd 3.14159265    
+three: dd 3.0     
+
 section .text
 global volume
 volume:
+	; xmm0 radius
+	; xmm1 height
+	divss xmm1, [rel three]
+	mulss xmm0, xmm0
+	mulss xmm0, [rel pi]
+	mulss xmm0, xmm1
  	ret
