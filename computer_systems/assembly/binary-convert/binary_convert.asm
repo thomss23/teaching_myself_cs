7429369303925a7b6055a75ab5a59614671864aa
ThomasB <borbelithomas@gmail.com>
2026-07-25 17:11:25 +0000

    add the solution for the binary convert exercise in assembly

---
+++ computer_systems/assembly/binary-convert/binary_convert.asm
@@ -0,0 +1,19 @@
+section .text
+global binary_convert
+; my solution
+binary_convert:
+mov eax, 0
+.loop:
+    cmp byte [rdi], 0
+	je .loop_end
+	movzx ecx, byte [rdi]  
+	sub ecx, '0' 
+	shl eax, 1
+	or eax, ecx
+	add rdi, 1
+	jmp .loop
+.loop_end:
+	ret
+
+; instructor solution
+
