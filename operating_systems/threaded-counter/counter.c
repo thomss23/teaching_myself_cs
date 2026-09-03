0831579e438b6d7519d13db1f4ca160fda454125
ThomasB <borbelithomas@gmail.com>
2026-09-03 20:15:47 +0000

    add thread counter exercise

---
+++ operating_systems/threaded-counter/counter.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <pthread.h>
+
+#define EACH_COUNT 1000000000
+pthread_mutex_t lock = PTHREAD_MUTEX_INITIALIZER;
+
+volatile int counter = 0;
+
+void* thread_entry(void *arg) {
+ pthread_mutex_lock(&lock);
+  for (int i = 0; i < EACH_COUNT; i++) {
+    counter++;
+  }
+  pthread_mutex_unlock(&lock);
+  return NULL;
+}
+
+int main () {
+  pthread_t p1, p2;
+  pthread_create(&p1, NULL, thread_entry, NULL);
+  pthread_create(&p2, NULL, thread_entry, NULL);
+  pthread_join(p1, NULL);
+  pthread_join(p2, NULL);
+  printf("Final count: %d (expected %d)\n", counter, 2 * EACH_COUNT);
+}
+
