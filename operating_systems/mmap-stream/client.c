6b0d7f91adf830e612a690499ac3dd7352944292
ThomasB <borbelithomas@gmail.com>
2026-09-15 13:28:32 +0000

    add my solution for the client-server sharing memory exercise

--- operating_systems/mmap-stream/client.c
+++ operating_systems/mmap-stream/client.c
@@ -1,40 +1,66 @@
 #include <arpa/inet.h>
+#include <fcntl.h>
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/mman.h>
 #include <sys/socket.h>
+#include <semaphore.h>
 #include <time.h>
+
+#define SHM_NAME "/number_shm"
+#define DATA_READY_SEM "/number_ready"
+#define BUFFER_FREE_SEM "/number_buffer_free"
 
 #define SIZE (1 << 20) // Test with this many bytes of data
 
 int main () {
-  int checksum, max, sock, n;
-  unsigned short port = 9876;
+  int n, checksum;
   struct timespec start, end;
-  struct sockaddr_in server;
+  int max = SIZE / sizeof(int);
 
-  max = SIZE / sizeof(int);
+  int fd = shm_open(SHM_NAME, O_RDONLY, 0666);
+  if (fd == -1) {
+    perror("shm_open (start the server first)");
+    return EXIT_FAILURE;
+  }
+  int *shared = mmap(NULL, SIZE, PROT_READ, MAP_SHARED, fd, 0);
+  if (shared == MAP_FAILED) {
+    perror("mmap");
+    return EXIT_FAILURE;
+  }
 
-  // construct the client socket, and connect
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&server, 0, sizeof(server));
-  server.sin_family = AF_INET;
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
-  server.sin_port = htons(port);
-  connect(sock, (struct sockaddr *)&server, sizeof(server));
+  sem_t *data_ready = sem_open(DATA_READY_SEM, 0);
+  sem_t *buffer_free = sem_open(BUFFER_FREE_SEM, 0);
+  if (data_ready == SEM_FAILED || buffer_free == SEM_FAILED) {
+    perror("sem_open (start the server first)");
+    return EXIT_FAILURE;
+  }
 
   // receive a bunch of data
   clock_gettime(CLOCK_MONOTONIC, &start);
   checksum = 0;
-  for (int i = 0; i < max; i++) {
-    recv(sock, &n, 4, 0);
-    checksum ^= n;
+
+  while(1) {
+
+    sem_wait(data_ready);
+
+    checksum = 0;
+    for (int i = 0; i < max; i++) {
+      n = shared[i];
+      checksum ^= n;
+    }
+    
+    clock_gettime(CLOCK_MONOTONIC, &end);
+
+    float secs =
+        (float)(end.tv_nsec - start.tv_nsec) / 1e9 + (end.tv_sec - start.tv_sec);
+    float mibs = (float)SIZE / secs / (1 << 20);
+
+    printf("Received at %.3f MiB/s. Checksum: %d\n", mibs, checksum);
+
+    // The server may now overwrite the buffer with the next batch.
+    sem_post(buffer_free);
   }
-  clock_gettime(CLOCK_MONOTONIC, &end);
 
-  float secs =
-      (float)(end.tv_nsec - start.tv_nsec) / 1e9 + (end.tv_sec - start.tv_sec);
-  float mibs = (float)SIZE / secs / (1 << 20);
-
-  printf("Received at %.3f MiB/s. Checksum: %d\n", mibs, checksum);
 }
--- operating_systems/mmap-stream/server.c
+++ operating_systems/mmap-stream/server.c
@@ -1,39 +1,103 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <sys/socket.h>
+#include <fcntl.h>
 #include <string.h>
 #include <arpa/inet.h>
+#include <sys/mman.h>
+#include <semaphore.h>
+#include <unistd.h>
+
+#define SHM_NAME "/number_shm"
+#define DATA_READY_SEM "/number_ready"
+#define BUFFER_FREE_SEM "/number_buffer_free"
 
 #define SIZE (1 << 20) // Test with this many bytes of data
 
 int main () {
-  int checksum, max, sock, conn, n;
-  unsigned short port = 9876;
-  struct sockaddr_in self, client;
-  unsigned int clientlen;
 
   srand(0x1234);
-  max = SIZE / sizeof(int);
+  int max = SIZE / sizeof(int);
+  int n;
+  int checksum;
 
-  // construct the server socket, bind and listen for connections
-  sock = socket(AF_INET, SOCK_STREAM, 0);
-  memset(&self, 0, sizeof(self));
-  self.sin_family = AF_INET;
-  self.sin_addr.s_addr = htonl(INADDR_ANY);
-  self.sin_port = htons(port);
-  bind(sock, (struct sockaddr *)&self, sizeof(self));
-  listen(sock, 10);
+  // Start a fresh shared-memory session.  Do this only when no old client is
+  // still using these names.
+  shm_unlink(SHM_NAME);
+  sem_unlink(DATA_READY_SEM);
+  sem_unlink(BUFFER_FREE_SEM);
 
-  // accept a new connection and stream it a bunch of random integers
+  int fd = shm_open(SHM_NAME, O_CREAT | O_RDWR, 0666);
+  if (fd == -1) {
+    perror("shm_open");
+    return EXIT_FAILURE;
+  }
+  if (ftruncate(fd, SIZE) == -1) {
+    perror("ftruncate");
+    return EXIT_FAILURE;
+  }
+
+  int *shared = mmap(NULL, SIZE, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
+  if (shared == MAP_FAILED) {
+    perror("mmap");
+    return EXIT_FAILURE;
+  }
+  close(fd);
+
+  sem_t *data_ready = sem_open(DATA_READY_SEM, O_CREAT, 0666, 0);
+  sem_t *buffer_free = sem_open(BUFFER_FREE_SEM, O_CREAT, 0666, 1);
+  if (data_ready == SEM_FAILED || buffer_free == SEM_FAILED) {
+    perror("sem_open");
+    return EXIT_FAILURE;
+  }
+
   for (;;) {
-    conn = accept(sock, (struct sockaddr *)&client, &clientlen);
-    printf("Connection accepted\n");
+    // Do not overwrite the buffer until the client has read the last batch.
+    sem_wait(buffer_free);
+
     checksum = 0;
     for (int i = 0; i < max; i++) {
       n = rand();
       checksum ^= n;
-      send(conn, &n, 4, 0);
+      shared[i] = n;
+      
     }
+
     printf("Sent %d random ints to client, checksum %d\n", max, checksum);
+    sem_post(data_ready);
   }
+
 }
+
+
+// int main () {
+//   int checksum, max, sock, conn, n;
+//   unsigned short port = 9876;
+//   struct sockaddr_in self, client;
+//   unsigned int clientlen;
+
+//   srand(0x1234);
+//   max = SIZE / sizeof(int);
+
+//   // construct the server socket, bind and listen for connections
+//   sock = socket(AF_INET, SOCK_STREAM, 0);
+//   memset(&self, 0, sizeof(self));
+//   self.sin_family = AF_INET;
+//   self.sin_addr.s_addr = htonl(INADDR_ANY);
+//   self.sin_port = htons(port);
+//   bind(sock, (struct sockaddr *)&self, sizeof(self));
+//   listen(sock, 10);
+
+//   // accept a new connection and stream it a bunch of random integers
+//   for (;;) {
+//     conn = accept(sock, (struct sockaddr *)&client, &clientlen);
+//     printf("Connection accepted\n");
+//     checksum = 0;
+//     for (int i = 0; i < max; i++) {
+//       n = rand();
+//       checksum ^= n;
+//       send(conn, &n, 4, 0);
+//     }
+//     printf("Sent %d random ints to client, checksum %d\n", max, checksum);
+//   }
+// }
